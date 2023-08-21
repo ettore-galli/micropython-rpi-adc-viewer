b31c75dc90c9904f55f244b407c93746d2a6a6e3
ettore-galli <ettore.galli@gmail.com>
2023-08-21 22:01:24 +0200

    Fast refresh

--- src/main.py
+++ src/main.py
@@ -19,8 +19,7 @@ class ADCMonitor:
         self,
         adc_value_logger,
         hardware_information: HardwareInformation = HardwareInformation(),
-        adc_delay_ms: float = 1,
-        refresh_delay_ms: float = 1,
+        adc_delay_ms: int = 1,
     ):
         self.adc_value_logger = adc_value_logger
         self.hardware_information = hardware_information
@@ -29,17 +28,10 @@ class ADCMonitor:
         self.adc = ADC(Pin(hardware_information.adc_gpio_pin))
         self.adc_value = 0
 
-        self.refresh_delay_ms = refresh_delay_ms
         self.display = self.display_setup(hardware_information=hardware_information)
 
         self.display_init(self.display)
         self.draw_init()
-
-        self.last_displayed_value: float = 0
-
-        self.pixels_num = 100
-        self.pixels = [0] * self.pixels_num
-        self.pixels_index = 0
 
     def display_setup(self, hardware_information: HardwareInformation):
         i2c = I2C(
@@ -67,77 +59,34 @@ class ADCMonitor:
     def draw_init(self):
         self.display.text("Value", 5, 5, 1)
         self.display.show()
-        print(self.display.buffer)
 
-    def display_value(self):
-        value = self.get_adc_value()
-        rect_height = 60
-        rect_left = 5
-        rect_top = 25
-        pixels_top = 120
-
-        def to_pixels(value):
-            return int(value * pixels_top / 65536)
-
-        display_pixels = to_pixels(value)
-
-        self.display.fill_rect(rect_left, rect_top, display_pixels, rect_height, 1)
-        self.display.fill_rect(
-            rect_left + display_pixels,
-            rect_top,
-            self.hardware_information.display_width - (rect_left + display_pixels),
-            rect_height,
-            0,
-        )
-
-        self.display.show()
-
-        self.displayed_value = value
-
-    def draw_wave_pixel_to_framebuffer(self, frame_buffer, value):
+    async def single_screen_loop(self):
         left_start = 5
         bottom_line = 62
-
         pixels_top = 40
+        pixels_per_screen = 100
+
+        frame_buffer = self.display
 
         def to_pixels(value):
             return int(value * pixels_top / 65536)
 
-        display_pixels = to_pixels(value)
-
-        left = left_start + self.pixels_index
-
-        frame_buffer.pixel(left, bottom_line - self.pixels[self.pixels_index], 0)
-        frame_buffer.pixel(left, bottom_line - display_pixels, 1)
-
-        return display_pixels
-
-    def display_wave(self):
-        value = self.get_adc_value()
-
-        display_pixels = self.draw_wave_pixel_to_framebuffer(
-            frame_buffer=self.display, value=value
+        self.display.fill_rect(
+            left_start, bottom_line - pixels_top, pixels_per_screen, pixels_top + 1, 0
         )
-        self.display.show()
 
-        self.pixels[self.pixels_index] = display_pixels
-        self.pixels_index = (self.pixels_index + 1) % self.pixels_num
-
-        self.displayed_value = value
-
-    async def adc_loop(self):
-        while True:
+        for position in range(pixels_per_screen):
             value = self.adc.read_u16()
-            self.set_adc_value(value)
-            self.display_wave()
+            value_in_pixels = to_pixels(value)
+            left = left_start + position
+            frame_buffer.pixel(left, bottom_line - value_in_pixels, 1)
             await asyncio.sleep_ms(self.adc_delay_ms)
 
-    async def display_change_loop(self):
+        self.display.show()
+
+    async def screen_loop(self):
         while True:
-            # adc_value = self.get_adc_value()
-            # self.adc_value_logger(adc_value)
-            self.display_wave()
-            await asyncio.sleep_ms(self.refresh_delay_ms)
+            await self.single_screen_loop()
 
 
 def render_value(value: float, top: float, stars: int):
@@ -161,6 +110,4 @@ async def main(coroutines):
 
 if __name__ == "__main__":
     adcm = ADCMonitor(adc_value_logger=log_adc_value)
-
-    # asyncio.run(main([adcm.display_change_loop, adcm.adc_loop]))  #  type: ignore
-    asyncio.run(main([adcm.adc_loop]))  #  type: ignore
+    asyncio.run(main([adcm.screen_loop]))  #  type: ignore
